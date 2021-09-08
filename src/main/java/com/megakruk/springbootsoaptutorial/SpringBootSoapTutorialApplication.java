96b76928e2895d30c34c5295fb83cd61b65933f4
MegaKruk <filip.mazur.dev@gmail.com>
2021-09-08 22:36:33 +0200

    Initial commit

---
+++ src/main/java/com/megakruk/springbootsoaptutorial/SpringBootSoapTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.megakruk.springbootsoaptutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSoapTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSoapTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/megakruk/springbootsoaptutorial/SpringBootSoapTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.megakruk.springbootsoaptutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSoapTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
